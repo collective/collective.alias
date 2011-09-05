976506eb7dbd4288937c611bd78284b5744697ee
vincentfretin <vincentfretin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-09-05 08:49:50 +0000

    clear and rebuild zc.relation catalog

--- src/collective/alias/upgrade.py
+++ src/collective/alias/upgrade.py
@@ -2,24 +2,28 @@ from plone.uuid.handlers import addAttributeUUID
 
 from zc.relation.interfaces import ICatalog
 from zope.component import getUtility
-from zope.event import notify
-from zope.lifecycleevent import ObjectModifiedEvent
+from z3c.relationfield.event import updateRelations
+from z3c.relationfield.interfaces import IHasRelations
 
 from Products.CMFCore.utils import getToolByName
 
 from collective.alias.interfaces import IAlias
 
 
-def add_uuid(context):
+def reindex_relations(context):
     rcatalog = getUtility(ICatalog)
     # Clear the relation catalog to fix issues with interfaces that don't exist anymore.
     # This actually fixes the from_interfaces_flattened and to_interfaces_flattened indexes.
-    # The ObjectModifiedEvent takes care of reindexing the relations only for aliases.
-    # /!\ If you have other dexterity content types with relations, you have to
-    # write a script that notify an ObjectModifiedEvent for each instance to
-    # reindex the relations.
     rcatalog.clear()
 
+    catalog = getToolByName(context, 'portal_catalog')
+    brains = catalog.searchResults(object_provides=IHasRelations.__identifier__)
+    for brain in brains:
+        obj = brain.getObject()
+        updateRelations(obj, None)
+
+
+def add_uuid(context):
     catalog = getToolByName(context, 'portal_catalog')
     brains = catalog.searchResults(object_provides=IAlias.__identifier__)
     for brain in brains:
@@ -31,7 +35,6 @@ def add_uuid(context):
                 obj.unrestrictedTraverse(('talkback', comment_id)).unindexObject()
         except AttributeError: # no talkback
             pass
-        notify(ObjectModifiedEvent(obj))
 
 
 def upgrade_actions(context):
