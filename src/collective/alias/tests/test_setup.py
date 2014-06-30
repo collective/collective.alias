0a305a884bc07fee7d238049edef53130a293cf8
Roel Bruggink <roel@jaroel.nl>
2014-06-30 21:44:14 +0200

    Fix tests, we're using Collection instead of Topic

--- src/collective/alias/tests/test_setup.py
+++ src/collective/alias/tests/test_setup.py
@@ -10,21 +10,21 @@ from collective.alias.interfaces import IAliasSettings
 
 
 class TestSetup(PloneTestCase):
-    
+
     layer = Layer
-    
+
     def test_type_installed(self):
         tt = getToolByName(self.portal, 'portal_types')
         self.failUnless('collective.alias' in tt.objectIds())
-    
+
     def test_action_installed(self):
         at = getToolByName(self.portal, 'portal_actions')
         self.failUnless('paste_alias' in at['object_buttons'])
-    
+
     def test_registry_records_installed(self):
         reg = getUtility(IRegistry)
         records = reg.forInterface(IAliasSettings)
-        self.failUnless('Topic' in records.traversalTypes)
+        self.failUnless('Collection' in records.traversalTypes)
         self.failUnless('view' in records.aliasActions)
 
 def test_suite():
